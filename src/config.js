bb1931e173493a1b33258181f49628fc083c84b9
quranyamal <mal.qurany@gmail.com>
2019-10-24 08:27:31 +0700

    init aws amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "ap-southeast-1",
+      BUCKET: "udanotesapp"
+    },
+    apiGateway: {
+      REGION: "ap-southeast-1",
+      URL: "https://onlluaa4lf.execute-api.ap-southeast-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "ap-southeast-1",
+      USER_POOL_ID: "ap-southeast-1_0eYCfJVsa",
+      APP_CLIENT_ID: "6hik8uglnk5r16970pfaqn04ii",
+      IDENTITY_POOL_ID: "ap-southeast-1:00919cb3-d1d5-439e-8acf-1c15ea892da7"
+    }
+  };
