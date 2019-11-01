4870aee07661e793637df70d70d24fa6f01a6c12
quranyamal <mal.qurany@gmail.com>
2019-11-01 08:04:34 +0700

    add the session to the state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import Routes from "./Routes";
 import "./App.css";
 
-function App(props) {
+var isAuthenticated = false
+var userHasAuthenticated = false
+
+function App(props)
+ {
+  [isAuthenticated, userHasAuthenticated] = useState(false);
   return (
     <div className="App container">
       <Navbar fluid collapseOnSelect>
@@ -17,18 +22,27 @@ function App(props) {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
-            <LinkContainer to="/signup">
-              <NavItem>Signup</NavItem>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <NavItem>Login</NavItem>
-            </LinkContainer>
+            {isAuthenticated
+              ? <NavItem onClick={handleLogout}>Logout</NavItem>
+              : <>
+                  <LinkContainer to="/signup">
+                    <NavItem>Signup</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <NavItem>Login</NavItem>
+                  </LinkContainer>
+                </>
+            }
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Routes />
+      <Routes appProps={{ isAuthenticated, userHasAuthenticated }} />
     </div>
   );
+}
+
+function handleLogout() {
+  userHasAuthenticated(false);
 }
 
 export default App;
