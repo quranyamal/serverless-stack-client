bf09d4b1a6096188677fa877ee8b355ff2f9e2d5
quranyamal <mal.qurany@gmail.com>
2019-11-01 21:16:48 +0700

    add signup

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 import NotFound from "./containers/NotFound";
 import AppliedRoute from "./components/AppliedRoute";
 
@@ -10,6 +11,7 @@ export default function Routes({ appProps }) {
     <Switch>
       <AppliedRoute path="/" exact component={Home} appProps={appProps} />
       <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+      <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
 
       { /* Finally, catch all unmatched routes */ }
       <Route component={NotFound} />
