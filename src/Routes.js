7ff8c277e5b6bdf66244e3d9ec7926c339c3943c
quranyamal <mal.qurany@gmail.com>
2019-11-02 14:10:07 +0700

    implement edit and delete notes functionality

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
+import Notes from "./containers/Notes";
 import NewNote from "./containers/NewNote";
 import NotFound from "./containers/NotFound";
 import AppliedRoute from "./components/AppliedRoute";
@@ -14,6 +15,7 @@ export default function Routes({ appProps }) {
       <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
       <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
       <AppliedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
+      <AppliedRoute path="/notes/:id" exact component={Notes} appProps={appProps} />
 
       { /* Finally, catch all unmatched routes */ }
       <Route component={NotFound} />
